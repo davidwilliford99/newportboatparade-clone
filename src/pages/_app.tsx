589f17470185bffc6b76af22c28fef7242c4d310
davidwilliford99 <d9899w@gmail.com>
2023-09-18 11:48:21 -0400

    Make background of web application white by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+// Div makes app have white background by default
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-white'>
+      <Component {...pageProps} />
+    </div>
+    
+  )
 }
